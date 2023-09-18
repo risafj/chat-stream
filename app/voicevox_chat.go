fb629e5a4ff5ec6e8e454a052cbb1068b1ff082b
risafj <risaf1231@gmail.com>
2023-09-18 16:20:27 +0900

    feat: Playing audio clip from device works, but not when looped

--- app/voicevox_chat.go
+++ app/voicevox_chat.go
@@ -3,7 +3,9 @@ package main
 import (
 	"io"
 	"log"
-	"os"
+	"time"
+
+	"github.com/ebitengine/oto/v3"
 )
 
 func voiceChat(voiceClient *VoiceVoxClient, chatClient *ChatGPTClient) {
@@ -17,22 +19,65 @@ func voiceChat(voiceClient *VoiceVoxClient, chatClient *ChatGPTClient) {
 		if err != nil {
 			log.Fatalf("Error getting audio: %v", err)
 		}
-		outputFile, err := os.Create("output.wav")
+
+		err = playAudio(audioClip)
 		if err != nil {
-			log.Fatalf("Error creating the output file: %v", err)
-			return
+			log.Fatalf("Error playing audio: %v", err)
 		}
-		// Copy the response body to the output file
-		_, err = io.Copy(outputFile, audioClip)
-		if err != nil {
-			log.Fatalf("Error copying the response body to the output file: %f", err)
-			return
-		}
-		err = outputFile.Close()
-		if err != nil {
-			log.Fatalf("Error closing the file: %f", err)
-			return
-		}
+
+		//outputFile, err := os.Create("output.wav")
+		//if err != nil {
+		//	log.Fatalf("Error creating the output file: %v", err)
+		//	return
+		//}
+		//// Copy the response body to the output file
+		//_, err = io.Copy(outputFile, audioClip)
+		//if err != nil {
+		//	log.Fatalf("Error copying the response body to the output file: %f", err)
+		//	return
+		//}
+		//err = outputFile.Close()
+		//if err != nil {
+		//	log.Fatalf("Error closing the file: %f", err)
+		//	return
+		//}
 		log.Printf("Response: %s\n", output)
 	}
+}
+
+func playAudio(audioClip io.Reader) error {
+	// Prepare an Oto context (this will use your default audio device) that will
+	// play all our sounds. Its configuration can't be changed later.
+	op := &oto.NewContextOptions{}
+
+	// Usually 44100 or 48000. Other values might cause distortions in Oto
+	op.SampleRate = 24000
+
+	// Number of channels (aka locations) to play sounds from. Either 1 or 2.
+	// 1 is mono sound, and 2 is stereo (most speakers are stereo).
+	op.ChannelCount = 1
+
+	// Format of the source. go-mp3's format is signed 16bit integers.
+	// NOTE: wav also uses 16bit integers.
+	op.Format = oto.FormatSignedInt16LE
+
+	otoCtx, readyChan, err := oto.NewContext(op)
+	if err != nil {
+		return err
+	}
+	// It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
+	<-readyChan
+
+	// Create a new 'player' that will handle our sound. Paused by default.
+	player := otoCtx.NewPlayer(audioClip)
+	player.Play()
+
+	// We can wait for the sound to finish playing using something like this
+	for player.IsPlaying() {
+		time.Sleep(time.Millisecond)
+	}
+
+	// If you don't want the player/sound anymore simply close
+	err = player.Close()
+	return err
 }
